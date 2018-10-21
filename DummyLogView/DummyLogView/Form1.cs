228b2ad7c969cb8742fe20535c09bbf2f6ac6bb1
kkcmh <kkcmh@126.com>
2018-10-21 23:35:22 +0800

    temp

---
+++ DummyLogView/DummyLogView/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Permissions;
+
+namespace DummyLogView
+{
+    public partial class Form1 : Form
+    {
+
+       
+
+        public static FileSystemWatcher watcher = new FileSystemWatcher();
+        public static FileSystemWatcher upWatchher = new FileSystemWatcher();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSetDownHost(object sender, EventArgs e)
+        {
+
+         
+        }
+
+        [PermissionSetAttribute(SecurityAction.Demand, Name = "FullTrust")]
+        private void OnDialogLoad(object sender, EventArgs e)
+        {
+
+           
+
+            watcher.Path = @"C:\kkcmh\";
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastAccess 
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName 
+                                 | NotifyFilters.DirectoryName;
+         
+            // Only watch text files.
+            watcher.Filter = "test.txt";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+
+            upWatchher.Path = @"C:\kkcmh\";
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            upWatchher.NotifyFilter = NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.DirectoryName;
+
+            // Only watch text files.
+            upWatchher.Filter = "uphost.txt";
+            upWatchher.Changed += new FileSystemEventHandler(OnHostChanged);
+
+            // Begin watching.
+            upWatchher.EnableRaisingEvents = true;
+         
+        }
+
+
+        public bool _LoadTxtOneLines(RichTextBox rcb , string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            TextReader tr = new StreamReader(fs, Encoding.Default);
+            string str = "";
+            rcb.Text = "";
+            while (str != null)
+            {
+                str = tr.ReadLine();
+                if(str!=null)
+                    rcb.AppendText(str + "\n");
+            }
+
+            return true;
+        }
+
+        private  void OnChanged(object source, FileSystemEventArgs e)
+        {
+
+            //watcher.EnableRaisingEvents = false;
+           // MessageBox.Show(e.FullPath);
+            string file = e.FullPath;
+            _LoadTxtOneLines(this.richTextBox1,file);
+
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+
+           // watcher.EnableRaisingEvents = true;
+           // Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            //_LoadTxtOneLines(e.FullPath);
+            
+        }
+
+
+        private void OnHostChanged(object source, FileSystemEventArgs e)
+        {
+
+            //watcher.EnableRaisingEvents = false;
+            // MessageBox.Show(e.FullPath);
+            string file = e.FullPath;
+
+            _LoadTxtOneLines(this.richTextBox2, file);
+            richTextBox2.SelectionStart = richTextBox2.TextLength;
+            richTextBox2.ScrollToCaret();
+
+            // watcher.EnableRaisingEvents = true;
+            // Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            //_LoadTxtOneLines(e.FullPath);
+
+        }
+
+        private void OnSetUpHost(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LogViewer/LogViewer/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LogViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitKeyWord()
+        {
+           // keywords.Add("1114");
+        }
+
+        private string readFile(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read,FileShare.ReadWrite);
+            TextReader tr = new StreamReader(fs, Encoding.Default);
+            string str = tr.ReadToEnd(); 
+            return str; 
+        }
+
+
+        private bool _LoadTxtOneLines(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            TextReader tr = new StreamReader(fs, Encoding.Default);
+            string str = "";
+            while (str != null)
+            {
+                str = tr.ReadLine();
+                richTextBox1.AppendText(str + "\n");
+            }
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //richTextBox1.Text = readFile(@"C:\kkcmh\test.txt");
+            _LoadTxtOneLines(@"C:\kkcmh\test.txt");
+            string findstr = "14";
+            int lineStartPos = 0;
+
+            foreach (string item in richTextBox1.Lines)
+            {
+                int index = item.IndexOf(findstr);
+                if(index != -1 )
+                { 
+                    this.richTextBox1.Select(lineStartPos,item.Length );
+                    richTextBox1.SelectionFont = new Font("宋体", 14, (FontStyle.Regular));
+                    richTextBox1.SelectionColor = Color.Red;
+                }
+
+                lineStartPos += item.Length + 1;
+            }
+        }
+
+    }
+}
